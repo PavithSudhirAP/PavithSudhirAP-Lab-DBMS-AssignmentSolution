233e3a9c367cc86f281ae94279dfe9be8c5b232c
Pavith Sudhir <56886753+PavithSudhirAP@users.noreply.github.com>
2022-05-23 21:41:03 +0530

    Add files via upload

---
+++ 04Question.sql
@@ -0,0 +1,6 @@
+select cus_name, pro_name, ord_id, ord_date, ord_amount from customer as c, product p, supplier_pricing as sp, 
+`order` as o 
+where c.cus_id = '2'
+and c.cus_id = o.cus_id
+and p.pro_id = sp.pro_id
+and o.pricing_id = sp.pricing_id;
